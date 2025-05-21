5f4f61a9dc81c186e77b29b36b4aeb7ea6dfbb80
Ilias McAuliffe <iliasmcauliffe@gmail.com>
2025-05-21 19:10:17 +0200

    initial centroid merging

--- clustering/embedding.py
+++ clustering/embedding.py
@@ -6,7 +6,9 @@ from transformers import AutoModel, AutoTokenizer
 from core.chain import ListChains
 from core.clusterer import Clusterer
 from core.constants import IdCluster
+from core.utils import get_logger
 
+logger = get_logger()
 
 class EmbeddingCluster(Clusterer):
 
@@ -14,8 +16,10 @@ class EmbeddingCluster(Clusterer):
         self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
         self.model = AutoModel.from_pretrained("bert-base-uncased")
         self.model.eval()
+        self.embeddings = None
+        self.centroids = None
 
-    def compute_embedding(self, thought: str) -> torch.Tensor:
+    def _compute_embedding(self, thought: str) -> torch.Tensor:
         """Takes a thought of a cot of a chain, and returns the embedding"""
         with torch.no_grad():
             inputs = self.tokenizer(thought, return_tensors="pt", truncation=True, max_length=128)
@@ -23,7 +27,7 @@ class EmbeddingCluster(Clusterer):
             embedding = outputs.last_hidden_state.mean(dim=1).squeeze(0)
         return embedding
     
-    def kmeans(self, chain_embeddings: list[torch.Tensor], n_iter: int = 3) -> list[int]:
+    def _kmeans(self, chain_embeddings: list[torch.Tensor], n_iter: int = 3) -> list[int]:
         """
         Simple k-means clustering for a list of 1D torch tensors.
         Returns a list of cluster assignments (same length as embeddings).
@@ -32,13 +36,25 @@ class EmbeddingCluster(Clusterer):
         X = np.array(chain_embeddings) # need to convert to numpy for sklearn
         # NOTE: always halfs number of chains, can also specify in the constructor instead
         k = len(chain_embeddings) // 2 + 1 
-        labels = KMeans(n_clusters=k, random_state=0, n_init="auto").fit_predict(X)
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto")
+        kmeans.fit(X)
+        labels = kmeans.predict(X)
 
-        # 2. Change to IdCluster format
+        # 2. Change to IdCluster format and store centroids
         km_clusters: list[IdCluster] = [[] for _ in range(k)]
+        self.centroids = [None for _ in range(k)]
         for i, cluster in enumerate(labels):
             km_clusters[cluster].append(i)
+            self.centroids[cluster] = torch.from_numpy(kmeans.cluster_centers_[i])
+        # logger.info(f"Centroids have shape {self.centroids.shape}")
+        logger.info(self.centroids)
         return km_clusters
+    
+    def get_centroids(self):
+        return self.centroids
+    
+    def get_embeddings(self):
+        return self.embeddings
 
     def __call__(self, chains: ListChains) -> list[IdCluster]:
         """Computes an embedding for each chain, and then clusters the chains"""
@@ -48,13 +64,17 @@ class EmbeddingCluster(Clusterer):
             cot_steps: list[str] = chain.get_generated_steps()
             thought_embeddings = list()
             for thought in cot_steps:
-                thought_embedding = self.compute_embedding(thought)
+                thought_embedding = self._compute_embedding(thought)
                 thought_embeddings.append(thought_embedding)
             # Aggregate embeddings in a chain by mean
             chain_embedding = torch.stack(thought_embeddings, dim=0).mean(dim=0)
             chain_embeddings.append(chain_embedding)
 
+        self.embeddings = chain_embeddings # TODO: rename
+
         # 2. Cluster the embeddings
-        clusters = self.kmeans(chain_embeddings)
+        clusters = self._kmeans(chain_embeddings)
+        logger.info(f"Type of centroids is {self.centroids[0].shape}")
+        logger.info(f"Type of embeddings is {self.embeddings[0].shape}")
         return clusters
 
