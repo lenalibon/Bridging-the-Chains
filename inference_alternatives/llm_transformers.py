699aefa1d1123fe40cd1be2ef493e3b0a8b545a5
Ilias Mc Auliffe <imcauliffe@student-cluster.inf.ethz.ch>
2025-06-11 15:47:51 +0200

    added basic kv cache

--- inference_alternatives/llm_transformers.py
+++ inference_alternatives/llm_transformers.py
@@ -1,9 +1,8 @@
 # pip install accelerate
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, DynamicCache
 import torch
-import os
 import time
-from prompting.prompts import GEMMA_SIMPLE_PROMPT_TEMPLATE
+from prompting.prompts import GEMMA_SIMPLE_PROMPT_TEMPLATE, GEMMA_MULTISHOT_PROMPT_TEMPLATE
 
 start_time = time.time()
 quantization_config = BitsAndBytesConfig(load_in_8bit=True)
@@ -17,15 +16,19 @@ model = AutoModelForCausalLM.from_pretrained(
 
 load_time = time.time()
 print(f"Time to load: {load_time - start_time}")
-input_text = GEMMA_SIMPLE_PROMPT_TEMPLATE.substitute(
+input_text = GEMMA_MULTISHOT_PROMPT_TEMPLATE.substitute(
     question="Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?")
 input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
-outputs = model.generate(**input_ids, return_dict_in_generate=True, output_scores=True)
+past_key_values = DynamicCache()
+outputs = model.generate(**input_ids, return_dict_in_generate=True, output_scores=True, max_new_tokens=256, past_key_values=past_key_values, use_cache=True)
 print("All outputs")
 print(tokenizer.decode(outputs['sequences'][0]))
 print(f"Inference time: {time.time() - load_time}")
 
-# TODO: check if the new prompt format helps https://ai.google.dev/gemma/docs/core/prompt-structure
-# TODO: check if i can use some KV cache
-# (maybe) TODO: switch to vLLM
+##### TODO:
+# - integrate with rest of code
+# - switch to vLLM
+
+##### DONE:
+# - check if the new prompt format helps https://ai.google.dev/gemma/docs/core/prompt-structure
